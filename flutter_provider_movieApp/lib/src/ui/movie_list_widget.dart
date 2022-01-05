e87d1e1fe330bfef7453edd8ae752fff16a6d3b5
Jinuk <jitoor610@gmail.com>
2022-01-05 23:01:52 +0900

    [0105] Flutter_Provider_MovieApp - 05

--- flutter_provider_movieApp/lib/src/ui/movie_list_widget.dart
+++ flutter_provider_movieApp/lib/src/ui/movie_list_widget.dart
@@ -1,18 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_provider/src/model/movie.dart';
 import 'package:flutter_provider/src/provider/movie_provider.dart';
 import 'package:provider/provider.dart';
 
 class MovieListWidget extends StatelessWidget {
-  const MovieListWidget({Key? key}) : super(key: key);
+  MovieListWidget({Key? key}) : super(key: key);
+  Widget _makeListView(List<Movie> movies) {
+    return ListView.separated(
+      itemCount: movies.length,
+      itemBuilder: (BuildContext context, int index) {
+        return Center(
+          child: Text(movies[index].title),
+        );
+      },
+      separatorBuilder: (BuildContext context, int index) {
+        return Divider();
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
+    MovieProvider _movieProvider =
+        Provider.of<MovieProvider>(context, listen: false);
+    _movieProvider.loadMovies;
     return Scaffold(
       appBar: AppBar(
         title: Text("Movie Provider"),
       ),
-      body: Consumer<MovieProvider>(builder: (context, provider, widget) {
-        if (provider.movies != null && provider.movies.length > 0) {}
+      body: Consumer<MovieProvider>(
+        builder: (context, provider, widget) {
+        if (provider.movies != null && provider.movies.length > 0) {
+          return _makeListView(provider.movies);
+        }
         return Center(
           child: CircularProgressIndicator(),
         );
